50913bed36f9db868e3edad31460c5224f52f060
jonathan-crepeau <jon.crepeau@gmail.com>
2022-09-22 11:35:54 -0700

    main, c#current-container & #forecast-container v1 complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log("Cloudy with a chance of meatballs.");
+// console.log("Cloudy with a chance of meatballs.");
+
+// async function requestWeather() {
+//     const httpRequest = await $.ajax({
+//         method: "GET",
+//         // url: "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/sananselmo,ca?include=days,current,fcst&key=3D6EZXHLSVULQWKNPQNZGMSNW",
+//         url: "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/sananselmo,ca?key=3D6EZXHLSVULQWKNPQNZGMSNW",
+//         success: function(response){
+//             postDescription(response);
+//         }
+//     })
+// }
+
+function postDescription(obj) {
+    console.log(obj);
+    let mySection = $('section');
+    $(mySection).html(obj.description)
+}
